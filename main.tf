ef78c4e7794236388304f394ae4f1507f4a146ee
Mauricio Mitolo <mauricio.estebanmitolo@hashicorp.com>
2024-07-04 11:26:37 +0200

    update ami reference

--- main.tf
+++ main.tf
@@ -8,6 +8,23 @@ resource "aws_vpc" "my_vpc" {
   }
 }
 
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "172.16.10.0/24"
@@ -50,7 +67,7 @@ module "ec2" {
 
   name = random_pet.server_name.id
 
-  ami                         = "ami-ebd02392"
+  ami                         = data.aws_ami.ubuntu.id
   instance_type               = "t2.micro"
   monitoring                  = true
   associate_public_ip_address = true
