e0b05849b9ef1ca4e178b1763a77e55a62253403
Mauricio Mitolo <mauricio.estebanmitolo@hashicorp.com>
2024-07-04 10:56:10 +0200

    update resource references

--- main.tf
+++ main.tf
@@ -18,11 +18,10 @@ resource "aws_subnet" "my_subnet" {
   }
 }
 
-
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = aws_vpc.my_vpc
+  vpc_id      = aws_vpc.my_vpc.id
 
   tags = {
     Name = "allow_tls"
@@ -31,7 +30,7 @@ resource "aws_security_group" "allow_tls" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = aws_vpc.main.cidr_block
+  cidr_ipv4         = aws_vpc.my_vpc.cidr_block
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
@@ -39,7 +38,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
   security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv6         = aws_vpc.main.ipv6_cidr_block
+  cidr_ipv6         = aws_vpc.my_vpc.ipv6_cidr_block
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
