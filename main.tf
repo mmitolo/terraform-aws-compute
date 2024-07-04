50defdc1425439dd9d6b221e8a717fabdb91ffa5
Mauricio Mitolo <mauricio.estebanmitolo@hashicorp.com>
2024-07-04 11:50:36 +0200

    clean and update random resource

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
-resource "random_pet" "server_name" {}
+resource "random_pet" "server_name" {
+  keepers = {
+    # Generate a new pet name each time we switch to a new AMI id
+    ami_id = data.aws_ami.ubuntu
+  }
+}
 
 resource "aws_vpc" "my_vpc" {
   cidr_block = "172.16.0.0/16"
@@ -48,14 +53,6 @@ resource "aws_security_group" "allow_tls" {
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   security_group_id = aws_security_group.allow_tls.id
   cidr_ipv4         = aws_vpc.my_vpc.cidr_block
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
-}
-
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv6         = aws_vpc.my_vpc.ipv6_cidr_block
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
